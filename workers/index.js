17b3b062bf32e910cf18838c3cffd824173713a4
Kate Hudson <k88hudson@gmail.com>
2014-07-08 13:20:48 -0400

    Bug 1030885 - Removed create event emailer

--- workers/index.js
+++ workers/index.js
@@ -16,7 +16,6 @@ module.exports = function(config) {
   return {
     mailer: require("./mailer")(mailerTransport),
     send_new_user_email: require("./create_user_emailer")(mailer),
-    send_event_host_email: require("./create_event_emailer")(mailer),
     sign_up_for_bsd: require("./sign_up_for_bsd"),
     send_mofo_staff_email: require("./send_mofo_staff_email")(config.mofoStaffEmail, mailer),
     badge_awarded_send_email: require("./badge_awarded_send_email")(mailer),
