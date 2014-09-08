88442a6b7f2f84425a704f28445be8c2ddb51a78
Jon Buckley <jon@jbuckley.ca>
2014-09-08 16:01:28 -0400

    Bug 1061948 - Use sqs-processor module

--- processor.js
+++ processor.js
@@ -2,19 +2,14 @@ if (process.env.NEW_RELIC_ENABLED) {
   require("newrelic");
 }
 
-var async = require("async");
-var AWS = require("aws-sdk");
+var SQSProcessor = require("sqs-processor");
 
-var debug = function() {
-  if (process.env.DEBUG === 'true') {
-    console.log.apply(null, arguments);
-  }
-}
-
-var queue = new AWS.SQS({
+var queue = new SQSProcessor({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_QUEUE_REGION
+  region: process.env.AWS_QUEUE_REGION,
+  queueUrl: process.env.INCOMING_QUEUE_URL,
+  debug: process.env.DEBUG === 'true'
 });
 
 var workers = require("./workers")({
@@ -25,39 +20,13 @@ var workers = require("./workers")({
   googlePassword: process.env.GOOGLE_PASSWORD
 });
 
-async.forever(function(callback) {
-  debug("requesting a message");
-  queue.receiveMessage({
-    MaxNumberOfMessages: 1,
-    QueueUrl: process.env.INCOMING_QUEUE_URL,
-    WaitTimeSeconds: 20
-  }, function(receiveError, data) {
-    if (receiveError) {
-      receiveError.from = "queue.recieveMessage()";
-      return callback(receiveError);
-    }
-
-    if (!data || !Array.isArray(data.Messages) || data.Messages.length !== 1) {
-      debug("no messages found");
-      return callback();
-    }
-
-    var message, receiptHandle;
-    try {
-      message = JSON.parse(data.Messages[0].Body);
-      receiptHandle = data.Messages[0].ReceiptHandle
-    } catch (jsonError) {
-      jsonError.data = data.Messages[0].Body;
-      return callback(jsonError);
-    }
-
+queue.startPolling(
+  function worker(message, callback) {
     if (!workers[message.event_type]) {
       var workerNotFoundError = new Error("No worker available to process '" + message.event_type + "'");
       workerNotFoundError.data = message.data;
       return callback(workerNotFoundError);
     }
-
-    debug("processing %j", message);
 
     workers[message.event_type](message.data, function(workerError) {
       if (workerError) {
@@ -66,28 +35,23 @@ async.forever(function(callback) {
         return callback(workerError);
       }
 
-      debug("deleting a message");
-
-      queue.deleteMessage({
-        QueueUrl: process.env.INCOMING_QUEUE_URL,
-        ReceiptHandle: receiptHandle
-      }, function(deleteErr) {
-        if (deleteErr) {
-          deleteErr.from = "queue.deleteMessage()";
-          return callback(deleteErr);
-        }
-
-        debug("message deleted");
-
-        callback();
-      });
+      callback();
     });
-  });
-}, function(closeError) {
-  if (closeError) {
-    console.log("%j", closeError);
-    console.log("%s", closeError.stack);
+  },
+  function error(poll_error) {
+    console.log(poll_error);
+    console.log(poll_error.stack);
+    process.exit(1);
   }
+);
 
-  process.exit(1);
-});
+var shutdown_handler = function() {
+  queue.stopPolling(function() {
+    console.log("polling terminated");
+    process.exit(0);
+  });
+};
+
+// Heroku's dyno manager sends SIGTERM when requesting dyno shut down, Ctrl-C sends SIGINT
+process.on("SIGTERM", shutdown_handler);
+process.on("SIGINT", shutdown_handler);
